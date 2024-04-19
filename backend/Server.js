e47d8017d8ea26639b00c1f66f843acbcfff1d8a
avish <avishusa45@gmail.com>
2024-04-19 13:33:47 -0500

    Error handling

--- backend/Server.js
+++ backend/Server.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const app=express()
 const dbConfig=require("./Config/dbConfig");
 const portfolioRoute = require("./routes/portfolioRoute")
+app.use(express.json());
+
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://frabjous-parfait-15b9c9.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -10,8 +13,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
-app.use("https://avish-portfolio.onrender.com/api/portfolio",portfolioRoute);
+app.use("/api/portfolio",portfolioRoute);
 const port=process.env.PORT || 5000;
 const path = require("path");
 
