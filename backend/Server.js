87186ff423821bc2bebb1ebe6b41fbdce7bd497b
avish <avishusa45@gmail.com>
2024-04-19 13:28:25 -0500

    Adding CORS functionality

--- backend/Server.js
+++ backend/Server.js
@@ -3,8 +3,15 @@ require("dotenv").config();
 const app=express()
 const dbConfig=require("./Config/dbConfig");
 const portfolioRoute = require("./routes/portfolioRoute")
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://frabjous-parfait-15b9c9.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.json());
-app.use("/api/portfolio",portfolioRoute);
+app.use("https://avish-portfolio.onrender.com/api/portfolio",portfolioRoute);
 const port=process.env.PORT || 5000;
 const path = require("path");
 
